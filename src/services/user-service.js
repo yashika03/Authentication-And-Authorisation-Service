2c00ef92f169e19960ca9fadc9480e3083dfe908
Yashika <yb310jh@gmail.com>
2023-01-17 19:01:49 +0530

    Created create token and verify token service

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,4 +1,7 @@
 const UserRepository = require('../repository/user-repository');
+const jwt = require('jsonwebtoken');
+const {JWT_KEY} = require('../config/serverConfig');
+
 class UserService{
     constructor()
     {
@@ -11,6 +14,30 @@ class UserService{
             return user;
         } catch (error) {
             console.log("Something went wrong in the Service Layer");
+            throw error;
+        }
+    }
+    
+    async createToken(user)
+    {
+        try {
+            const response = await jwt.sign(sign,JWT_KEY,{
+                expiresIn: '1h'
+            });
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in creating the token");
+            throw error;
+        }
+    }
+
+    async verifyToken(token)
+    {
+        try {
+            const response = jwt.verify(token,JWT_KEY);
+            return response;
+        } catch (error) {
+            console.log("Something went wrong in validating the token",error);
             throw error;
         }
     }
